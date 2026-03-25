baf2828ddb8fe6be049cd9a85cc535fc36d89326
Simeon Simeonoff <sim.simeonoff@gmail.com>
2026-03-25 19:49:25 +0200

    refactor(scss): imporve indents

--- languages/scss/indents.scm
+++ languages/scss/indents.scm
@@ -1,3 +1,11 @@
-(_
-  "{"
-  "}" @end) @indent
+; Block indentation (CSS & SCSS constructs using { })
+(block "}" @end) @indent
+
+; Mixin blocks (used by @mixin and @include)
+(mixin_block "}" @end) @indent
+
+; Parenthesized groups (maps, lists, parameters, arguments)
+(map_value ")" @end) @indent
+(list_value ")" @end) @indent
+(parameters ")" @end) @indent
+(arguments ")" @end) @indent
